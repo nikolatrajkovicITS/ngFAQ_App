eadfd13225e14f234e760b7cb5373bb8e9c2acfe
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-12-29 21:53:25 +0100

    Setup Navbar

---
+++ src/app/components/question-list/question-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-list',
+  templateUrl: './question-list.component.html',
+  styleUrls: ['./question-list.component.css']
+})
+export class QuestionListComponent implements OnInit {
+  questions:Object[];
+
+  constructor() { 
+    this.questions = [
+      {
+        text:'What is your name?',
+        answer: 'My name is Brad'
+      },
+      {
+        text:'What is your favorite color?',
+        answer: 'My favorite color is red'
+      },
+      {
+        text:'What is your favorite language?',
+        answer: 'My favorite language is JavaScript'
+      }
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
