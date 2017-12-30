8b924db8c3a429edcd79435a086f968bdad93af1
Nikola Trajkovic <nikola.trajkovic@bluecouchdigital.com>
2017-12-30 23:53:31 +0100

    Answer toggle Data Service

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+import { Question } from '../../models/Question';
 
 @Component({
   selector: 'app-question-list',
@@ -6,26 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent implements OnInit {
-  questions:Object[];
+  questions:Question[];
 
-  constructor() { 
-    this.questions = [
-      {
-        text:'What is your name?',
-        answer: 'My name is Brad'
-      },
-      {
-        text:'What is your favorite color?',
-        answer: 'My favorite color is red'
-      },
-      {
-        text:'What is your favorite language?',
-        answer: 'My favorite language is JavaScript'
-      }
-    ];
+  constructor(public dataService:DataService) { 
+    
   }
 
   ngOnInit() {
+    this.questions = this.dataService.getQuestions();
   }
 
 }
